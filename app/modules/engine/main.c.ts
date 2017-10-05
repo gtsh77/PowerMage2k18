ad092678ce9cb9839d6944c937c0985bfe7544e8
Anton Makridin <a.i.makridin@yandex.ru>
2017-10-06 01:07:03 +0300

    main_c refactor

--- app/modules/engine/main.c.ts
+++ app/modules/engine/main.c.ts
@@ -2,14 +2,15 @@ import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-	template: '<canvas id="canvas" controls (p_controls_e)="controls($event)" [width]="viewportWidth" [height]="viewportHeight"></canvas>'
+	template: '<canvas id="canvas" controls (p_controls_e)="controls($event)" [width]="viewportWidth * factor" [height]="viewportHeight * factor"></canvas>'
 })
 
 export class main_c {
-	private viewportWidth: number = 500;
-	private viewportHeight: number = 250;
-	private mapWidth: number = 2500;
-	private mapHeight: number = 2500;
+	private debug: boolean = false;
+	private viewportWidth: number = 20;
+	private viewportHeight: number = 10;
+	private mapWidth: number = null;
+	private mapHeight: number = null;
 	private factor: number = 25;
 
 	private canvas: any = null;
@@ -20,119 +21,106 @@ export class main_c {
 	constructor(private route: ActivatedRoute){}
 
 	public ngOnInit(): void {
+		window.scope = this;
 		this.route.data.forEach((data: any ) => {
 			this.map = data.mapData.json();
+			this.mapHeight = this.mapWidth = Math.sqrt(this.map.length);
 		});
 	}
 
-	public ngAfterViewInit(): void {
-		window.scope = this;
+	public ngAfterViewInit(): void {		
 		this.canvas = document.querySelector('#canvas');
 		this.ctx = this.canvas.getContext('2d');
 		this.drawViewport();
 	}
 
 	public controls(direction: string): void {
-		let curplayerPos: number = this.map.indexOf(1);
-		let curplayerLine: number = Math.floor(curplayerPos/(this.mapWidth/this.factor));
-		let newPlayerPos: number = null;
-		let newPlayerLine: number = null;
+		let curplayerPos: number = this.map.indexOf(1),
+			curplayerLine: number = Math.floor(curplayerPos/this.mapWidth),
+			newPlayerPos: number = null,
+			newPlayerLine: number = null;
 
 		if(direction === 'left'){
 			newPlayerPos = curplayerPos - 1;
-			newPlayerLine = Math.floor(newPlayerPos/(this.mapWidth/this.factor));
+			newPlayerLine = Math.floor(newPlayerPos/this.mapWidth);
 			if(curplayerLine !== newPlayerLine) return;
 		} 
 		else if(direction === 'right'){
 			newPlayerPos = curplayerPos + 1;
-			newPlayerLine = Math.floor((newPlayerPos + 1)/(this.mapWidth/this.factor));
+			newPlayerLine = Math.floor((newPlayerPos + 1)/this.mapWidth);
 			if(curplayerLine !== newPlayerLine) return;			
 		} 
 		else if(direction === 'up'){
-			newPlayerPos = curplayerPos - this.mapWidth/this.factor;
+			newPlayerPos = curplayerPos - this.mapWidth;
 			if(newPlayerPos < 0) return;
 
 		} 
 		else if(direction === 'down'){
-			newPlayerPos = curplayerPos + this.mapWidth/this.factor;
-			newPlayerLine = Math.floor(newPlayerPos/(this.mapWidth/this.factor));
-			if(newPlayerLine + 2 > this.mapHeight/this.factor) return;	
+			newPlayerPos = curplayerPos + this.mapWidth;
+			newPlayerLine = Math.floor(newPlayerPos/this.mapWidth);
+			if(newPlayerLine + 2 > this.mapHeight) return;	
 		} 
 
 		if(this.map[newPlayerPos] === 0){
 			this.map[curplayerPos] = 0;
 			this.map[newPlayerPos] = 1;
-			this.clearField();
+			this.clearViewport();
 			this.drawViewport();
 		}
 	}
 
-	public clearField(): void {
+	public clearViewport(): void {
 		this.ctx.clearRect(0,0,this.viewportWidth,this.viewportHeight);
 	}
 
 	public drawViewport(): void {
-		//tmp fix for extend x2
-		let extendX2: boolean = false;
-		//tmp fix for extend upLines
-		let extendUpLines: boolean = false;
-		//get y
-		let y: number = this.map.indexOf(1);
-		//get curLine
-		let curLine: number = Math.floor(y/(this.mapWidth/this.factor));
-		//get x1
-		let x1: number = y - Math.floor(this.viewportWidth/this.factor/2);
-		if(Math.floor(x1/(this.mapWidth/this.factor)) !== curLine){
-			x1 = (this.mapWidth/this.factor) * curLine;
+		//calc pre-render staff
+		let extendX2: boolean = false,
+			extendUpLines: boolean = false,
+			y: number = this.map.indexOf(1),
+			curLine: number = Math.floor(y/this.mapWidth),
+			x1: number = y - Math.floor(this.viewportWidth/2),
+			x2: number = y + Math.floor(this.viewportWidth/2),
+			nDownLines: number = null,
+			nUpLines: number = null;
+
+		if(Math.floor(x1/this.mapWidth) !== curLine){
+			x1 = this.mapWidth * curLine;
 			extendX2 = true;
 		}
-		//get x2
-		let x2: number = y + Math.floor(this.viewportWidth/this.factor/2);
-		if(Math.floor(x2/(this.mapWidth/this.factor)) !== curLine){
-			x2 = (this.mapWidth/this.factor) * (curLine + 1) - 1;
-			//extend x1
-			x1 -= (Math.floor(this.viewportWidth/this.factor/2) - (x2 - y));
+		if(Math.floor(x2/this.mapWidth) !== curLine){
+			x2 = this.mapWidth * (curLine + 1) - 1;
+			x1 -= (Math.floor(this.viewportWidth/2) - (x2 - y));
 		}
-		//extend x2
 		if(extendX2){
-			//extend x2
-			x2 += (Math.floor(this.viewportWidth/this.factor/2) - (y - x1));
+			x2 += (Math.floor(this.viewportWidth/2) - (y - x1));
 		}
-		//get nDownLines
-		let nDownLines: number = null;
-		if((curLine + Math.floor(this.viewportHeight/this.factor/2)) * this.mapWidth/this.factor < this.map.length){
-			nDownLines = Math.floor(this.viewportHeight/this.factor/2);
+		if((curLine + Math.floor(this.viewportHeight/2)) * this.mapWidth < this.map.length){
+			nDownLines = Math.floor(this.viewportHeight/2);
 		}
 		else {
 			extendUpLines = true;
-			nDownLines = this.mapHeight/this.factor - curLine - 1;
+			nDownLines = this.mapHeight - curLine - 1;
 
 		}
-		//get nUpLines
-		let nUpLines: number = null;
-		if(Math.floor(this.viewportHeight/this.factor/2) < curLine){
-			nUpLines = Math.floor(this.viewportHeight/this.factor/2);
+		if(Math.floor(this.viewportHeight/2) < curLine){
+			nUpLines = Math.floor(this.viewportHeight/2);
 		}
 		else {
 			nUpLines = curLine;
-			//extend downLines
-			nDownLines += Math.floor(this.viewportHeight/this.factor/2) - nUpLines;
+			nDownLines += Math.floor(this.viewportHeight/2) - nUpLines;
 		}
-
-		//extend upLines
 		if(extendUpLines){
-			nUpLines += Math.floor(this.viewportHeight/this.factor/2) - nDownLines;
+			nUpLines += Math.floor(this.viewportHeight/2) - nDownLines;
 		}
 
-		console.log(curLine,y,x1,x2,nDownLines,nUpLines);
+		if(this.debug) console.log(curLine,y,x1,x2,nDownLines,nUpLines);
 
 		//start rendering
-		for(let i: number = nUpLines, i2: number = 0; i >= -nDownLines ;i--,i2+=this.factor){
-			let start: number = x1 - ((this.mapWidth/this.factor) * i);
-			let end: number = x2 - ((this.mapWidth/this.factor) * i);
-			for(let j: number = start, j2: number = 0; j <= end; j++,j2+=this.factor){
-				//console.log('draw',i,j);
-				//define is there obj?
+		for(let i: number = nUpLines, i2: number = 0; i >= -nDownLines; i--, i2 += this.factor){
+			let start: number = x1 - (this.mapWidth * i);
+			let end: number = x2 - (this.mapWidth * i);
+			for(let j: number = start, j2: number = 0; j <= end; j++, j2 += this.factor){
 				if(this.map[j] !== 0){
 					if(this.map[j] === 2) this.ctx.fillStyle = 'black';
 					else if(this.map[j] === 1) this.ctx.fillStyle = 'red';
@@ -144,4 +132,8 @@ export class main_c {
 	}
 }
 
-declare var window: any;
+declare let window: windowplus;
+
+interface windowplus extends Window {
+	scope: main_c;
+}
